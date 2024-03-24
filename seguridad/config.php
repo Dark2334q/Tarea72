4f04971f70a3d801e9a7ff7b4335f2811e3c0bb7
Dark2334q <164710016+Dark2334q@users.noreply.github.com>
2024-03-24 00:22:54 -0500

    Update config.php

--- seguridad/config.php
+++ seguridad/config.php
@@ -1,7 +1,5 @@
 <?php
-$conn=mysqli_init();
 session_start();
-mysqli_ssl_set($conn,null,null,"PRACTICAS2/seguridad/DigiCertGlobalRootCA.crt.pem",null,null);
 $host = "dark12.mysql.database.azure.com";
 $user = "Admin12";
 $password = "Darkady1";
@@ -10,5 +8,4 @@ $con = mysqli_connect($host, $user, $password, $dbname);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-    return $conn;
 ?>
